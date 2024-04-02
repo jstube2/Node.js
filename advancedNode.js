20710151032e69f795d9903527b30f0d3cabc849
jstube2 <jstube2@gmail.com>
2024-04-02 10:15:57 -0500

    Update advancedNode.js

--- advancedNode.js
+++ advancedNode.js
@@ -3,3 +3,9 @@ http.createServer(function (req, res) {
     res.write('hello from server');//write a response to the client
     res.end();//end of response from server
 }).listen(6000);//the server instance listens for http requests on port 6000
+
+const fs = require('fs');
+// Synchronously read the file 'sample.txt' and store its contents in 'data'
+const data = fs.readFileSync('sample.txt', 'utf8');
+// Print the contents of 'sample.txt' to the console
+console.log(data);
