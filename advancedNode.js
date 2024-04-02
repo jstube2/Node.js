259bea2e53d7f10cdfa922df782482eb8e6e71fa
jstube2 <jstube2@gmail.com>
2024-04-02 10:17:46 -0500

    Update advancedNode.js

--- advancedNode.js
+++ advancedNode.js
@@ -9,3 +9,33 @@ const fs = require('fs');
 const data = fs.readFileSync('sample.txt', 'utf8');
 // Print the contents of 'sample.txt' to the console
 console.log(data);
+
+const fs = require('fs');
+// Read the contents of the file '/content.md' synchronously and store them in 'data'
+const data = fs.readFileSync('/content.md', 'utf8');
+// Print the contents of 'content.md' to the console
+console.log(data);
+
+let os = require('os');
+console.log("Computer OS Platform Info : " + os.platform());
+console.log("Computer OS Architecture Info: " + os.arch());
+
+const path = require('path');
+let result = path.basename('/content/index/home.html');
+console.log(result); //outputs home.html to the console
+
+let util = require('util');
+let str = 'The loop has executed %d time(s).';
+for (let i = 1; i <= 10; i++) {
+    console.log(util.format(str, i)); //outputs 'The loop has executed i time(s)'
+}
+
+const url = require('url');
+let webAddress = 'http://localhost:2000/index.html?lastName=Kent&firstName=Clark';
+let qry = url.parse(webAddress, true);
+let qrydata = qry.query; //returns an object: {lastName: 'Kent', firstName: 'Clark'}
+console.log(qrydata.firstName); //outputs Clark
+
+let qry = require('querystring');
+let qryParams = qry.parse('lastName=Kent&firstName=Clark');
+console.log(qryParams.firstName); //returns Clark
